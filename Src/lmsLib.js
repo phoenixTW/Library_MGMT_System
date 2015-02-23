2ec22fe24c244f7a11ec22b141422ffcb4d888c8
Ananthu R V <ananthurv666@hotmail.com>
2015-02-23 16:20:24 +0530

    v0.0 | added feature to display the available copies number

--- Src/lmsLib.js
+++ Src/lmsLib.js
@@ -4,17 +4,30 @@ exports.countBooks = function(books) {
 	books.forEach(function(book){
 		var book_name = book.book_name;
 		var book_details = {};
-		
-		if (countOfBooks.length == 0)
-			return ((book_details.name = book_name) && (book_details.count = 1)) && countOfBooks.push(book_details);
+
+		if (countOfBooks.length == 0){
+			book_details.name = book_name;
+			book_details.count = 1;
+			book_details.available = book.available;
+			return countOfBooks.push(book_details);
+		}
 
 		var isExists = false;
 
 		countOfBooks.forEach(function (stat) {
-			(stat.name) && (stat.name == book_name) && stat.count++ && (isExists = true);
+			(stat.name)
+			 && (stat.name == book_name)
+			 && stat.count++
+			 && (stat.available += book.available) 
+			 && (isExists = true);
 		});
 
-		(!isExists) && ((book_details.name = book_name) && (book_details.count = 1)) && countOfBooks.push(book_details);
+		(!isExists) 
+			&& ((book_details.name = book_name)
+			&& (book_details.count = 1)
+			&& (book_details.available = book.available))
+				&& countOfBooks.push(book_details);
+
 	});
 
 	return countOfBooks;
--- test/lmsLibTest.js
+++ test/lmsLibTest.js
@@ -4,27 +4,27 @@ var assert = require('chai').assert;
 describe('lms_lib',function(){
 
 	describe('#countBooks',function(done){
-		it('should return [{name: "java", count :1}] for input [{"book_name":"java"}]',function(done){
-			expected = 	[{name: "java", count :1}];
-			assert.deepEqual(lib.countBooks([{"book_name":"java"}]),expected);
+		it('should return [{name: "java", count :1, available: 1}] for input [{"book_name":"java"}]',function(done){
+			expected = 	[{name: "java", count :1, available: 1}];
+			assert.deepEqual(lib.countBooks([{"book_name":"java", available: 1}]),expected);
 			done();
 		});
 
-		it('should return [{name: "java", count:2}] for input [{"book_name":"java"}, {"book_name":"java"}]',function(done){
-			expected = 	[{name: "java", count:2}];
-			assert.deepEqual(lib.countBooks([{"book_name":"java"}, {"book_name":"java"}]),expected);
+		it('should return [{name: "java", count:2, available: 2}] for input [{"book_name":"java", available: 1}, {"book_name":"java", available: 1}]',function(done){
+			expected = 	[{name: "java", count:2, available: 2}];
+			assert.deepEqual(lib.countBooks([{"book_name":"java", available: 1}, {"book_name":"java", available: 1}]),expected);
 			done();
 		});
 
-		it('should return [{name: "java", count:2},{name: "javar", count:1}] for input [{"book_name":"java"}, {"book_name":"java"}, {"book_name":"javar"}]',function(done){
-			expected = 	[{name: "java", count:2},{name: "javar", count:1}];
-			assert.deepEqual(lib.countBooks([{"book_name":"java"}, {"book_name":"java"}, {"book_name":"javar"}]),expected);
+		it('should return [{name: "java", count:2, available: 1},{name: "javar", count:1, available: 1}] for input [{"book_name":"java", available: 1}, {"book_name":"java", available: 0}, {"book_name":"javar", available: 1}]',function(done){
+			expected = 	[{name: "java", count:2, available: 1},{name: "javar", count:1, available: 1}];
+			assert.deepEqual(lib.countBooks([{"book_name":"java", available: 1}, {"book_name":"java", available: 0}, {"book_name":"javar", available: 1}]),expected);
 			done();
 		});
 
-		it('should return [{name: "java", count:2},{name: "SQL", count:2}] for input [{"book_name":"java"}, {"book_name":"java"}, {"book_name":"SQL"}, {"book_name":"SQL"}]',function(done){
-			expected = 	[{name: "java", count:2},{name: "SQL", count:2}];
-			assert.deepEqual(lib.countBooks([{"book_name":"java"}, {"book_name":"java"}, {"book_name":"SQL"}, {"book_name":"SQL"}]),expected);
+		it('should return [{name: "java", count:2, available: 0},{name: "SQL", count:2, available: 2}] for input [{"book_name":"java", available: 0}, {"book_name":"java", available: 0}, {"book_name":"SQL", available: 1}, {"book_name":"SQL", available:1}]',function(done){
+			expected = 	[{name: "java", count:2, available: 0},{name: "SQL", count:2, available: 2}];
+			assert.deepEqual(lib.countBooks([{"book_name":"java", available: 0}, {"book_name":"java", available: 0}, {"book_name":"SQL", available: 1}, {"book_name":"SQL", available:1}]),expected);
 			done();
 		});
 	});
