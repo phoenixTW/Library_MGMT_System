82268595d4996ff9162ea4dd4d66eadb3a7c6db1
Pooja <poojag@thoughtworks.com>
2015-02-23 16:15:56 +0530

    User Search is completed

--- Src/lmsModule.js
+++ Src/lmsModule.js
@@ -41,11 +41,10 @@ var _getSearchedBooks = function(name,db,onComplete){
 	var searchQry = new JsSql();
 	searchQry.select();
 	searchQry.from(["books"]);
-	searchQry.where(["book_name='"+name+"'"]);
+	searchQry.where(["book_name like '%"+name+"%'"]);
 	searchQry.ready(db,"all",onComplete);
 	searchQry.fire();
 }
-
 
 var init = function(location){
 	var operate = function(operation){
@@ -65,7 +64,8 @@ var init = function(location){
 	};
 
 	var records = {	
-		getSearchedBooks : operate(_getSearchedBooks)
+		getSearchedBooks : operate(_getSearchedBooks),
+		// canBooksReturnByUser: operate(_canBooksReturnByUser)
 	};
 	return records;
 };
--- test/lmsTest.js
+++ test/lmsTest.js
@@ -12,11 +12,20 @@ describe('lms_records',function(){
 	});
 
 	describe('#getSearchedTopics',function(){
-		it('getSearchedTopics gives the all related books which is searched',function(done){
-			expected = "JAVA";
+		it('getSearchedTopics gives the all related books which book name is java',function(done){
+			var expected = "JAVA";
 			lms_records.getSearchedBooks('JAVA',function(err,topics){
 				assert.notOk(err);
 				assert.deepEqual(topics[0].book_name,expected);
+				done();
+			});
+		});
+
+		it('getSearchedTopics gives the all related books which is searched',function(done){
+			lms_records.getSearchedBooks('JAVA',function(err,topics){
+				assert.notOk(err);
+				assert.deepEqual(topics, [ { id: 12345, book_name: 'JAVA', available: 1, takenBy: null },
+					{ id: 12346, book_name: 'Javascript Reference', available: 0, takenBy: 1 } ])
 				done();
 			});
 		});
