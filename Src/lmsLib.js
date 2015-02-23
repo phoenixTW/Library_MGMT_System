62b5a98d4ef7a72a3015b709f1cd7458a717edcc
Ananthu R V <ananthurv666@hotmail.com>
2015-02-23 14:08:50 +0530

    v0.0 | failing test passed for count books method

--- Src/lmsLib.js
+++ Src/lmsLib.js
@@ -1,8 +1,8 @@
 exports.countBooks = function(books) {
 	var countOfBooks = {};
-	console.log('------------->>>', books);
+
 	books.forEach(function(book){
-		(Object.keys(countOfBooks).indexOf(book.book_names)>-1)
+		(Object.keys(countOfBooks).indexOf(book.book_name)>-1)
 			? countOfBooks[book.book_name]++ : countOfBooks[book.book_name] = 1;
 	});
 
