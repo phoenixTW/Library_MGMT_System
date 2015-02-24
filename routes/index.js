beeea06c26f3b83cdfc0a537317c2f91c7764ed2
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-02-24 18:19:55 +0530

    v0.0 | index.js is edited

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var lib = require('../Src/lmsModule').init("./data/lms.db");
 var lmsLib = require('../Src/lmsLib');
 
+
 router.get(['/', '/login'], function(req, res) {
   res.render('index', { title: 'router' });
 });
